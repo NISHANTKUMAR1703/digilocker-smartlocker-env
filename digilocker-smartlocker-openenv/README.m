6a1f9e48549e1eec6194c47d7ea2a43264c866ad
NISHANTKUMAR1703 <nc3345657@gmail.com>
2026-04-09 23:16:21 +0530

    Update and rename README.md to README.m

--- digilocker-smartlocker-openenv/README.m
+++ digilocker-smartlocker-openenv/README.m
@@ -1,3 +1,13 @@
+---
+title: DigiLocker SmartLocker Env
+emoji: 🔐
+colorFrom: blue
+colorTo: purple
+sdk: docker
+app_file: app.py
+pinned: false
+---
+
 # DigiLocker SmartLocker OpenEnv
 
 ## Description
